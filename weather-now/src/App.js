5d998858efb8cafb0d3a98213235a4c0bab0fa5e
dark-tracer <bernieamponsah2@gmail.com>
2024-10-14 13:15:43 +0000

    Combine components

--- weather-now/src/App.js
+++ weather-now/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import WeatherDashboard from './WeatherDashboard';
+import Forecast from './Forecast';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar />
+      <WeatherDashboard />
+      <Forecast />
     </div>
   );
 }
